b992cde16400e47e29cf1b753a6dccdc2f41108a
Feeph Aifeimei <55798703+feeph@users.noreply.github.com>
2026-07-07 12:35:22 +0200

    refactor: remove example tests from 'libs/ripit/src/lib.rs'

--- libs/ripit/src/lib.rs
+++ libs/ripit/src/lib.rs
@@ -10,18 +10,3 @@ mod unshackle;
 pub use eject::eject_medium;
 pub use scan::{ScanResult, scan_disc};
 pub use unshackle::{UnshackleResult, unshackle_disc};
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
